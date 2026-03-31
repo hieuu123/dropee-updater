f3247fa5069824d8083df3b5245fd12f0b754633
hieuu123 <140571471+hieuu123@users.noreply.github.com>
2026-03-31 17:14:19 +0700

    Update quiz_update.py

--- quiz_update.py
+++ quiz_update.py
@@ -8,11 +8,11 @@ from bs4 import BeautifulSoup, NavigableString, Tag
 WP_URL = "https://blog.mexc.fm/wp-json/wp/v2/posts"
 WP_USERNAME = os.getenv("WP_USERNAME")
 WP_APP_PASSWORD = os.getenv("WP_APP_PASSWORD")
-POST_ID = 326879  # <-- đổi thành ID bài Dropee cần update
-CHECK_ANSWER = "Ecommerce"  # <-- đáp án cũ dùng để đối chiếu
+POST_ID = 327368  # <-- đổi thành ID bài Dropee cần update
+CHECK_ANSWER = "Debt"  # <-- đáp án cũ dùng để đối chiếu
 
 SOURCE1_URL = "https://miningcombo.com/dropee/"
-SOURCE2_URL = "https://www.quiknotes.in/dropee-question-of-the-day-30-march-2026/"
+SOURCE2_URL = "https://www.quiknotes.in/dropee-question-of-the-day-1-april-2026/"
 
 # ================= HELPERS =================
 def normalize(text: str) -> str:
